ec4ad0173d350cf8bb6117737b606f9facf1dff2
meatpuppet <jan@kwoh.de>
2015-05-21 19:45:19 +0200

    init

---
+++ ubik.py
@@ -0,0 +1,100 @@
+__author__ = 'meatpuppet'
+
+import sys
+import logging
+import getpass
+from optparse import OptionParser
+import queue
+
+from xmpp_bot import XmppBot
+from irc_testbot import IrcBot
+
+if sys.version_info < (3, 0):
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+else:
+    raw_input = input
+
+
+irc_networks = {}
+
+if __name__ == '__main__':
+    # Setup the command line arguments.
+    optp = OptionParser()
+
+    # Output verbosity options.
+    optp.add_option('-q', '--quiet', help='set logging to ERROR',
+                    action='store_const', dest='loglevel',
+                    const=logging.ERROR, default=logging.INFO)
+    optp.add_option('-d', '--debug', help='set logging to DEBUG',
+                    action='store_const', dest='loglevel',
+                    const=logging.DEBUG, default=logging.INFO)
+    optp.add_option('-v', '--verbose', help='set logging to COMM',
+                    action='store_const', dest='loglevel',
+                    const=5, default=logging.INFO)
+
+    # JID and password options.
+    optp.add_option("-j", "--jid", dest="jid",
+                    help="JID to use")
+    optp.add_option("-p", "--password", dest="password",
+                    help="password to use")
+
+    opts, args = optp.parse_args()
+
+    # Setup logging.
+    logging.basicConfig(level=opts.loglevel,
+                        format='%(levelname)-8s %(message)s')
+
+    if opts.jid is None:
+        opts.jid = raw_input("Username: ")
+    if opts.password is None:
+        opts.password = getpass.getpass("Password: ")
+
+    # Setup the EchoBot and register plugins. Note that while plugins may
+    # have interdependencies, the order in which you register them does
+    # not matter.
+    xmpp_bot = XmppBot(opts.jid, opts.password, in_queue=queue.Queue(), out_queue=queue.Queue())
+    xmpp_bot.register_plugin('xep_0030') # Service Discovery
+    xmpp_bot.register_plugin('xep_0004') # Data Forms
+    xmpp_bot.register_plugin('xep_0060') # PubSub
+    xmpp_bot.register_plugin('xep_0199') # XMPP Ping
+
+    # If you are working with an OpenFire server, you may need
+    # to adjust the SSL version used:
+    # xmpp.ssl_version = ssl.PROTOCOL_SSLv3
+    #xmpp.XMPP_CA_CERT_FILE = None
+    xmpp_bot.reconnect_max_attempts=2
+    # If you want to verify the SSL certificates offered by a server:
+    #xmpp.ca_certs = "./kwoh.de.crt"
+
+
+
+    # Connect to the XMPP server and start processing XMPP stanzas.
+    if xmpp_bot.connect():
+        # If you do not have the dnspython library installed, you will need
+        # to manually specify the name of the server if it does not match
+        # the one in the JID. For example, to use Google Talk you would
+        # need to use:
+        #
+        # if xmpp.connect(('talk.google.com', 5222)):
+        #     ...
+        xmpp_bot.process(threaded=True, blocking=False)
+        #print("Done")
+    else:
+        print("Unable to connect.")
+    logging.info('going into loop...')
+    while True:
+        while not xmpp_bot.out_queue.empty():
+            cmd = xmpp_bot.out_queue.get(True, 0.1)
+            if cmd.get('command', None) == 'irc_connect':
+                logging.info('got connect to %s' % cmd.get('server'))
+                irc_networks[cmd.get('server')] = IrcBot(nick='ubik__', server=cmd.get('server'), port=6667, in_queue=queue.Queue(), logfile=cmd.get('server')+'.log')
+                irc_networks[cmd.get('server')].start()
+            if cmd.get('command', None) == 'irc_join':
+                # irc_networks[cmd.get('server')].join(cmd.get('room'))
+                logging.info('got join to %s' % cmd.get('room'))
+                irc_networks[cmd.get('server')].in_queue.put(
+                    {'command': 'join',
+                     'room': cmd.get('room')
+                     }
+                )
---
+++ xmpp_bot.py
@@ -0,0 +1,154 @@
+__author__ = 'meatpuppet'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    SleekXMPP: The Sleek XMPP Library
+    Copyright (C) 2010  Nathanael C. Fritz
+    This file is part of SleekXMPP.
+
+    See the file LICENSE for copying permission.
+"""
+
+import sys
+
+
+import sleekxmpp
+
+from irc_testbot import IrcBot
+import logging
+
+import queue
+
+# Python versions before 3.0 do not use UTF-8 encoding
+# by default. To ensure that Unicode is handled properly
+# throughout SleekXMPP, we will set the default encoding
+# ourselves to UTF-8.
+if sys.version_info < (3, 0):
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+else:
+    raw_input = input
+
+admins = ['jan@kwoh.de']
+
+
+
+
+class XmppBot(sleekxmpp.ClientXMPP):
+
+    """
+    A simple SleekXMPP bot that will echo messages it
+    receives, along with a short thank you message.
+    """
+
+    def __init__(self, jid, password, in_queue=None, out_queue=None):
+        sleekxmpp.ClientXMPP.__init__(self, jid, password)
+
+        # The session_start event will be triggered when
+        # the bot establishes its connection with the server
+        # and the XML streams are ready for use. We want to
+        # listen for this event so that we we can initialize
+        # our roster.
+        self.add_event_handler("session_start", self.start)
+
+        # The message event is triggered whenever a message
+        # stanza is received. Be aware that that includes
+        # MUC messages and error messages.
+        self.add_event_handler("message", self.message)
+
+        self.in_queue = in_queue
+        self.out_queue = out_queue
+
+        self.commands = {#'!test': self.command_test,
+                         '!help': self.command_help,
+                         '!irc_join': self.command_irc_join,
+                         '!irc_connect': self.command_irc_connect,
+                         '!irc': self.command_irc,
+                         '!irc_part': self.command_irc_part
+                         }
+
+
+    def start(self, event):
+        """
+        Process the session_start event.
+
+        Typical actions for the session_start event are
+        requesting the roster and broadcasting an initial
+        presence stanza.
+
+        Arguments:
+            event -- An empty dictionary. The session_start
+                     event does not provide any additional
+                     data.
+        """
+        self.send_presence()
+        self.get_roster()
+
+    def message(self, msg):
+        """
+        Process incoming message stanzas. Be aware that this also
+        includes MUC messages and error messages. It is usually
+        a good idea to check the messages's type before processing
+        or sending replies.
+
+        Arguments:
+            msg -- The received message stanza. See the documentation
+                   for stanza objects and the Message stanza to see
+                   how it may be used.
+        """
+        if msg['type'] in ('chat', 'normal'):
+            if not msg['from'].bare in admins:
+                #msg.reply("ok, you tested. msg:\n%(body)s" % msg).send()
+                logging.debug('message from %s (no admin)' % msg['from'].bare)
+                return
+            if msg['body'].startswith('!'):
+                if len(msg['body']) >= 1:
+                    command = msg['body'].split(' ')[0]
+                    if command in self.commands:
+                        self.commands[command](msg['from'].bare, msg)
+                    else:
+                        msg.reply("cmd not found...").send()
+
+    def command_irc(self, sender, msg):
+        self.send_message(mto=sender,
+                        mbody="irc networks: %s" % (','.join(self.irc_networks.keys())),
+                        mtype='chat')
+
+    def command_irc_connect(self, sender, msg):
+        if len(msg['body'].split(' ')) < 2:
+           msg.reply("usage: !irc_connect <server>").send()
+           return
+        server = msg['body'].split(' ')[1]
+        cmd = {
+            'command': 'irc_connect',
+            'server': server
+        }
+        self.out_queue.put(cmd)
+        msg.reply("connecting to %s!" % (server)).send()
+
+    def command_irc_join(self, sender, msg):
+        server = msg['body'].split(' ')[1]
+        cmd = {
+            'command': 'irc_join',
+            'server': server,
+            'room': msg['body'].split(' ')[2]
+        }
+        self.out_queue.put(cmd)
+
+    def command_irc_part(self, sender, msg):
+        server = msg['body'].split(' ')[1]
+        cmd = {
+            'command': 'irc_part',
+            'server': server,
+            'room': msg['body'].split(' ')[2]
+        }
+        self.out_queue.put(cmd)
+
+    def command_help(self, sender, msg):
+        self.send_message(mto=sender,
+                        mbody="available commands: %s" % (','.join(self.commands.keys())),
+                        mtype='chat')
+
+
+            #msg.reply("Thanks for sending\n%(body)s" % msg).send()
